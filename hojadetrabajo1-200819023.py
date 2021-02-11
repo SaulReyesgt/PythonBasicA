0f3d2e1848c5d88b9e2ea050d3bc9a464175eecc
SaulReyesgt <msaulreyesm@gmail.com>
2021-02-11 13:42:06 -0600

    Hojadetrabajo1-2008190223

---
+++ hojadetrabajo1-200819023.py
@@ -0,0 +1,20 @@
+#Marvin Saul Reyes Molina
+#200819023
+#Ejercicio 1
+n = int(input("Ingrese un número entero para el ejercicio 15: "))
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print("")
+
+
+#Ejercicio 2
+
+n = int(input("Introduce un número entero para el ejercicio 2: "))
+i = 1
+while n % i != 0:
+    i += 1
+if i == n:
+    print(str(n) + " es primo")
+else:
+    print(str(n) + " no es primo")
